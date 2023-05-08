eda1f17aca9a084dc881c6a9cd9accb404b9b9af
Hajime Nakagami <nakagami@gmail.com>
2023-05-08 14:43:16 +0900

    fix test

--- protocol/nla/cssp_test.go
+++ protocol/nla/cssp_test.go
@@ -10,7 +10,7 @@ import (
 func TestEncodeDERTRequest(t *testing.T) {
 	ntlm := nla.NewNTLMv2("", "", "")
 	result := nla.EncodeDERTRequest([]nla.Message{ntlm.GetNegotiateMessage()}, []byte{}, []byte{})
-	if hex.EncodeToString(result) != "302fa003020102a12830263024a02204204e544c4d53535000010000003582086000000000000000000000000000000000" {
+	if hex.EncodeToString(result) != "3037a003020102a130302e302ca02a04284e544c4d535350000100000035820860000000000000000000000000000000000000000000000000" {
 		t.Error("not equal")
 	}
 }
