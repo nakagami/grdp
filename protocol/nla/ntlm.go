5eee7eb22b3e953eac84b8c20cc4d4c845dfd743
Hajime Nakagami <nakagami@gmail.com>
2025-03-16 14:31:28 +0900

    remove log

--- protocol/nla/ntlm.go
+++ protocol/nla/ntlm.go
@@ -423,7 +423,6 @@ func (n *NTLMv2) GetAuthenticateMessage(s []byte) (*AuthenticateMessage, *NTLMv2
 	if challengeMsg.NegotiateFlags&NTLMSSP_NEGOTIATE_UNICODE != 0 {
 		n.enableUnicode = true
 	}
-	slog.Info(fmt.Sprintf("user: %s, passwd:%s", n.user, n.password))
 	domain, user, _ := n.GetEncodedCredentials()
 
 	n.authenticateMessage = NewAuthenticateMessage(challengeMsg.NegotiateFlags,
